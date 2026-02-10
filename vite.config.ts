81bca3b4ad13d684468442f11371f3488a5a7c00
Shi Zhang <iwtfyh2017@gmail.com>
2026-02-09 19:51:54 -0500

    deploy on GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FIXME: For GitHub Pages, set this to '/silcs-fragmaps-demo/'
-  base: '/',
+  // GitHub Pages base path: /repository-name/
+  base: '/silcs-fragmaps-demo/',
 })
